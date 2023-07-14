a42be186abf16d9f97048888802f724171b0f401
Kyle Williams <15004488+elykkyle@users.noreply.github.com>
2023-07-14 12:09:52 -0500

    revert cf logging policy

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -169,7 +169,25 @@ module "s3_cf_logging" {
   acl = "log-delivery-write"
 
   control_object_ownership = true
-  object_ownership         = "BucketOwnerPreferred"
+  object_ownership         = "ObjectWriter"
 
-  attach_access_log_delivery_policy = true
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Id": "S3-LOGGING-POLICY",
+    "Statement": [
+        {
+            "Sid": "S3-Allow-Logging",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "logging.s3.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "${module.s3_cf_logging.s3_bucket_arn}/*"
+        }
+    ]
+}
+EOF
+
+  attach_policy = true
 }
